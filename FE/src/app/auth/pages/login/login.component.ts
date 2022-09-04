fa0f639d22b21207f3c943203eaf21406fea733f
Agustín Casado <agu.e.casado@gmail.com>
2022-09-04 01:17:12 -0300

    servicio para validar token

--- FE/src/app/auth/pages/login/login.component.ts
+++ FE/src/app/auth/pages/login/login.component.ts
@@ -42,7 +42,5 @@ export class LoginComponent {
           }
         }
       );
-
   }
-
 }
--- FE/src/app/auth/services/auth.service.ts
+++ FE/src/app/auth/services/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, catchError, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
 import { AuthResponse } from '../interfaces/authResponse.interface';
 import { Usuario } from '../interfaces/usuario.interface';
+import { Token } from '@angular/compiler';
 
 
 
@@ -33,6 +34,8 @@ export class AuthService {
       .pipe(
         tap( resp => {
           if ( resp.ok ) {
+            localStorage.setItem('token', resp.token!);
+
             this._usuario = {
               name: resp.name!,
               uid: resp.uid!
@@ -42,5 +45,12 @@ export class AuthService {
         map( resp => resp.ok ),
         catchError( err => of( err.error.msg ))
       );
+  }
+
+  validarToken() {
+    const url  = `${ this.baseUrl }/auth/renew`;
+    const headers = new HttpHeaders()
+      .set('x-token', localStorage.getItem('token') || '');
+    return this.http.get( url, { headers } );
   }
 }
