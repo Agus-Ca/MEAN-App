babbd7e62d01a6f69f5d937d92a4210617378588
Agustín Casado <agu.e.casado@gmail.com>
2022-09-04 01:41:44 -0300

    validacion del token y guard de la pantalla

--- FE/src/app/auth/services/auth.service.ts
+++ FE/src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, catchError, tap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
 import { AuthResponse } from '../interfaces/authResponse.interface';
@@ -39,7 +39,7 @@ export class AuthService {
             this._usuario = {
               name: resp.name!,
               uid: resp.uid!
-            }
+            };
           }
         }),
         map( resp => resp.ok ),
@@ -47,10 +47,23 @@ export class AuthService {
       );
   }
 
-  validarToken() {
+  validarToken(): Observable<boolean> {
     const url  = `${ this.baseUrl }/auth/renew`;
     const headers = new HttpHeaders()
       .set('x-token', localStorage.getItem('token') || '');
-    return this.http.get( url, { headers } );
+    return this.http.get<AuthResponse>( url, { headers } )
+      .pipe(
+        map( resp => {
+          localStorage.setItem('token', resp.token!);
+
+          this._usuario = {
+            name: resp.name!,
+            uid: resp.uid!
+          };
+
+          return resp.ok;
+        }),
+        catchError( err => of( false ) )
+      );
   }
 }
